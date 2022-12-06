38cd74237da746d215bee0f89ad3c788b0528d74
Lucas Mateus Marson <lucas-marson@hotmail.com>
2022-12-06 10:41:01 -0300

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -12,14 +12,14 @@ import { Search } from "./pages/search";
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/busca" element={<Search />} />
       <Route path="/cardapio/:id" element={<Carte />} />
       <Route path="/solicitation" element={<Solicitations />} />
       <Route path="/favorites" element={<Favorites />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/adresses" element={<Adresses />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Login />} />
       <Route path="/cadastre" element={<Cadastre />} />
     </Routes>
   );
