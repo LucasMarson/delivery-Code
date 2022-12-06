321df60e59fda18a86deea7ad20e780af9d53dfd
Lucas Mateus Marson <lucas-marson@hotmail.com>
2022-12-06 10:47:46 -0300

    Update index.tsx

--- src/pages/cadastre/index.tsx
+++ src/pages/cadastre/index.tsx
@@ -104,7 +104,7 @@ export function Cadastre() {
       localStorage.setItem('sessionCodCidade', cod_cidade)
       localStorage.setItem('sessionCidade', cidade)
       localStorage.setItem('sessionUf', uf)
-      navigate('/')
+      navigate('/home')
     }).catch(err => console.error(err))
   }
   return (
